9c1433eb17458780790b032514a504d3a0dfd89d
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-24 13:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const element = document.getElementById('level');
+
+function findDOMLevel(element)
+	{
+		let level = 0;
+		let currEle = element;
+
+		while(currEle!== document.documentElement)
+		{
+			currEle = currEle.parentElement;
+			level++;
+		}
+
+		return level;
+	}
+
+const domLevel = findDOMLevel(element);
+
+alert("The level of the element is: " + domLevel);
